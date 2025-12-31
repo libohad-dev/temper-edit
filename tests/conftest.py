be7e6dba94ab469403376b4642be691970c6a4f2
Ohad Livne <libohad-dev@proton.me>
2025-12-31 21:22:39 +0200

    Run tests under multiple Python versions

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,15 +10,27 @@ from testcontainers.core.container import DockerContainer  # type: ignore[import
 from testcontainers.core.image import DockerImage  # type: ignore[import-untyped]
 
 
-@pytest.fixture
-def image() -> Iterator[DockerImage]:
+@pytest.fixture(
+    params=[
+        # keep-sorted start
+        "3.10.19-alpine3.23",
+        "3.11.14-alpine3.23",
+        "3.12.12-alpine3.23",
+        "3.13.11-alpine3.23",
+        "3.14.2-alpine3.23",
+        # keep-sorted end
+    ]
+)
+def image(request: pytest.FixtureRequest) -> Iterator[DockerImage]:
+    image_tag: str = request.param
     with DockerImage(
         path=Path(__file__).parent.parent,
         # keep-sorted start
+        buildargs={"IMAGE_TAG": image_tag},
         clean_up=False,
         dockerfile_path="tests/Containerfile",
         squash=True,
-        tag="temper-edit-test",
+        tag=f"temper-edit-test:{image_tag}",
         # keep-sorted end
     ) as test_image:
         yield test_image
