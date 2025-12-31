398b50769ed84d432c04dc40f58f940171a76eed
Ohad Livne <libohad-dev@proton.me>
2025-12-31 21:40:52 +0200

    Fix indentation of the injected script

--- tests/test_temp_file_creation.py
+++ tests/test_temp_file_creation.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import textwrap
 from collections.abc import Iterator
 from pathlib import Path
 from typing import cast
@@ -54,14 +55,14 @@ def test_failed_command(container: DockerContainer) -> None:
 def test_basic_tempfile_permissions(container: DockerContainer) -> None:
     tempfile = parse_output(container.exec(["mktemp"]))
 
-    script = f"""
+    script = textwrap.dedent(f"""\
     import os
     sr = os.stat("{tempfile}")
     print(sr.st_size)
     print(hex(sr.st_mode))
     print(sr.st_uid)
     print(sr.st_gid)
-    """
+    """)
     filestat = parse_output(container.exec(["python", "-c", script]))
 
     assert filestat.split() == ["0", "0x8180", "0", "0"]
