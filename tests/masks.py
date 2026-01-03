1494107be48d260acf2b96ec353c37b4ee56fcea
Ohad Livne <libohad-dev@proton.me>
2026-01-03 19:36:47 +0200

    Add bounding masks for mask manipulation utilities

--- tests/masks.py
+++ tests/masks.py
@@ -5,18 +5,32 @@
 from collections.abc import Iterator
 
 
-def submasks(mask: int) -> Iterator[int]:
+def submasks(mask: int, lower_bound: int = 0) -> Iterator[int]:
+    if mask & lower_bound != lower_bound:
+        raise ValueError(f"Lower bound {bin(lower_bound)} is not a submask of {bin(mask)}")
+
     submask = mask
     yield submask
-    while submask > 0:
+    while submask > lower_bound:
         submask = (submask - 1) & mask
-        yield submask
+        if submask & lower_bound == lower_bound:
+            yield submask
 
 
-def supermasks(mask: int, num_bits: int) -> Iterator[int]:
-    if mask >> num_bits != 0:
-        raise ValueError("Bound is too small")
+def all_bits(mask: int) -> int:
+    acc = mask
+    shift = 1
+    while delta := acc >> shift:
+        acc |= delta
+        shift <<= 1
 
-    all_bits = (1 << num_bits) - 1
-    for submask in submasks(mask ^ all_bits):
-        yield submask ^ all_bits
+    return acc
+
+
+def supermasks(mask: int, upper_bound: int) -> Iterator[int]:
+    if mask & upper_bound != mask:
+        raise ValueError(f"Upper bound {bin(upper_bound)} is not a supermask of {bin(mask)}")
+
+    full_mask = all_bits(upper_bound)
+    for submask in submasks(mask ^ full_mask, lower_bound=upper_bound ^ full_mask):
+        yield submask ^ full_mask
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -4,7 +4,7 @@
 
 import pytest
 
-from tests.masks import submasks, supermasks
+from tests.masks import all_bits, submasks, supermasks
 
 
 def test_null_submasks() -> None:
@@ -15,14 +15,37 @@ def test_non_null_submasks() -> None:
     assert list(submasks(0b1010)) == [10, 8, 2, 0]
 
 
-def test_insufficient_bound() -> None:
+def test_invalid_submask_lower_bound() -> None:
     with pytest.raises(ValueError):
-        _ = next(supermasks(0b101, num_bits=2))
+        next(submasks(0b101, lower_bound=0b110))
+
+
+def test_bound_submasks() -> None:
+    assert list(submasks(0b1101, lower_bound=0b1000)) == [13, 12, 9, 8]
+    assert list(submasks(0b1110, lower_bound=0b0100)) == [14, 12, 6, 4]
+
+
+def test_all_bits() -> None:
+    assert all_bits(0) == 0
+    assert all_bits(4) == 7
+    assert all_bits(5) == 7
+    assert all_bits(15) == 15
+
+
+def test_invalid_supermask_upper_bound() -> None:
+    with pytest.raises(ValueError):
+        _ = next(supermasks(0b101, upper_bound=0b100))
+    with pytest.raises(ValueError):
+        _ = next(supermasks(0b101, upper_bound=0b1000))
 
 
 def test_single_supermasks() -> None:
-    assert list(supermasks(0b111, num_bits=3)) == [7]
+    assert list(supermasks(0b111, upper_bound=0b111)) == [7]
 
 
 def test_multiple_supermasks() -> None:
-    assert list(supermasks(0b101, num_bits=4)) == [5, 7, 13, 15]
+    assert list(supermasks(0b101, upper_bound=0b1111)) == [5, 7, 13, 15]
+
+
+def test_bound_supermasks() -> None:
+    assert list(supermasks(0b1, upper_bound=0b1011)) == [1, 3, 9, 11]
