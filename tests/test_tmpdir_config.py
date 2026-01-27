ff2ac80562d8cc261f9abdbe2c45c6afa54cdfd5
Ohad Livne <libohad-dev@proton.me>
2026-01-27 23:49:39 +0200

    Test that the TMPDIR envvar is respected in sandbox creation

---
+++ tests/test_tmpdir_config.py
@@ -0,0 +1,56 @@
+# SPDX-FileCopyrightText: 2026 Ohad Livne <libohad-dev@proton.me>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import shlex
+import textwrap
+import uuid
+
+import pytest
+from testcontainers.core.container import DockerContainer  # type: ignore[import-untyped]
+
+from tests.utils import (
+    TEMPER_EDIT_SHELL_COMMAND,
+    extract_preserved_temporary_filename,
+    list_container_files,
+    parse_output,
+)
+
+
+def test_tmpdir_envvar_is_respected(container: DockerContainer) -> None:
+    # Fail after editing the temporary file
+    script = textwrap.dedent("""\
+    #! /bin/sh
+    echo "$1" > "$2"
+    exit 1
+    """)
+    container.exec(["sh", "-c", f"echo {shlex.quote(script)} > /tmp/edit-file"])
+    container.exec(["chmod", "u+x", "/tmp/edit-file"])
+
+    filename = "/tmp/file.txt"
+    container.exec(["touch", filename])
+
+    custom_tmpdir = "/tmp/custom-tmpdir"
+    container.exec(["mkdir", "-p", custom_tmpdir])
+
+    content = str(uuid.uuid4())
+    with pytest.raises(RuntimeError, match="Editor failed. Temporary file preserved at:") as exc_info:
+        _ = parse_output(
+            container.exec(
+                [
+                    "sh",
+                    "-c",
+                    f'TMPDIR="{custom_tmpdir}" EDITOR="/tmp/edit-file {content}" {TEMPER_EDIT_SHELL_COMMAND} {filename}',
+                ]
+            )
+        )
+
+    # Verify temporary file was created in the custom tmpdir, preserved, and contains the expected content
+    tempfile = extract_preserved_temporary_filename(exc_info)
+    assert list_container_files(container=container, directory="/tmp") == {
+        "/tmp/file.txt",
+        "/tmp/edit-file",
+        custom_tmpdir,
+    }
+    assert list_container_files(container=container, directory=custom_tmpdir) == {tempfile}
+    assert parse_output(container.exec(["cat", tempfile])) == content, "Preserved temp file has wrong content"
